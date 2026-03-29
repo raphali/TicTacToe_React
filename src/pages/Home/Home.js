5e2cf7746ef03d5af3f13dfac4abf93256b4df6d
Raphael <raphalizier2@gmail.com>
2026-03-29 21:07:55 +0200

    first commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import "../../styles/Home.css"
+
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
+function Reset({ onReset }) {
+  return <button className="reset-button" onClick={onReset}> Reset </button>;
+}
+
+function Victory(squares) {
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function Home() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    if (Victory(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function handleReset() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+  }
+
+  const winner = Victory(squares);
+  let status;
+  if (winner) {
+    status = winner + ' a gagné';
+    console.log(status);
+    return <p className="victory-message">{status}</p>;
+  } else if (squares.every(square => square !== null)) {
+    status = 'Match nul !';
+    console.log(status)
+    return <p className="victory-message">{status}</p>;
+  } else {
+    status = 'Prochain tour : ' + (xIsNext ? 'x' : 'o');
+    console.log(status)
+  }
+
+  return (
+    <>
+      <div className="boardRow1">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="boardRow2">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="boardRow3">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+
+      <div className="reset">
+        <Reset onReset={handleReset} />
+      </div>
+    </>
+
+  );
+}
+
+export default Home;
